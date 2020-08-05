1e57758d6632b923a67afa4a04bb0382a4f96cd9
shifangxue <shifangxue1986@163.com>
2020-08-05 17:03:01 +0800

    包

--- game/practice_game.py
+++ game/practice_game.py
@@ -7,40 +7,34 @@ my_hp = hp - enemy_power
 enemy_final_hp = enemy_hp - my_power
 谁的hp先为0，那么谁就输了
 """
-import random,time
-
-def skill(c_p, k_p):
-    if c_p > k_p:
-        print("\t\t\t空中百裂脚 <====> 疾风迅雷腿")
-    else:
-        print()
-
+import random
+import time
 def fight():
     # 初使化双方血量
     ken_hp = 1000
     chunli_hp = 1000
 
     while True:
-        #双方随机生成功击力
+        # 双方随机生成功击力
         ken_power = random.randint(40, 90)
         chunli_power = random.randint(40, 90)
-        #计算双方功击后剩余血量
+        # 计算双方功击后剩余血量
         ken_hp=ken_hp-chunli_power
         chunli_hp=chunli_hp-ken_power
-        #模仿延迟
+        # 模仿延迟
         time.sleep(0.5)
-        #打印双方血量信息及比赛结果
+        # 打印双方血量信息及比赛结果
         print("ken_hp:",ken_hp,"(-",chunli_power,")\t\tchunli_hp:",chunli_hp,"(-",ken_power,")")
-        skill(k_p=ken_power,c_p=chunli_power)
-        if ken_hp<=0:
+        # skill(k_p=ken_power,c_p=chunli_power)
+        if ken_hp <= 0:
             print("----------------------------")
             print("春丽胜利了")
             break
-        elif chunli_hp<=0:
+        elif chunli_hp <= 0:
             print("----------------------------")
             print("肯.马斯达斯胜利了")
             break
-        elif ken_hp==chunli_hp:
+        elif ken_hp == chunli_hp:
             print("----------------------------")
             print("同归于尽")
     print("============over==============")
