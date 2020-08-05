64b7d0f9218ec6e5ab6848659b8b67d86505aab5
shifangxue <shifangxue1986@163.com>
2020-08-05 17:25:21 +0800

    游戏项目

--- game/practice_game.py
+++ game/practice_game.py
@@ -15,6 +15,9 @@ def fight():
     ken_hp = 1000
     chunli_hp = 1000
 
+    print("ken_hp:",ken_hp,"\tchunli_hp:",chunli_hp)
+    print("============START==============")
+
     while True:
         # 双方随机生成功击力
         ken_power = random.randint(40, 90)
@@ -22,8 +25,8 @@ def fight():
         # 计算双方功击后剩余血量
         ken_hp=ken_hp-chunli_power
         chunli_hp=chunli_hp-ken_power
-        # 模仿延迟
-        time.sleep(0.5)
+        # 模似延迟
+        time.sleep(0.)
         # 打印双方血量信息及比赛结果
         print("ken_hp:",ken_hp,"(-",chunli_power,")\t\tchunli_hp:",chunli_hp,"(-",ken_power,")")
         # skill(k_p=ken_power,c_p=chunli_power)
@@ -35,10 +38,10 @@ def fight():
             print("----------------------------")
             print("肯.马斯达斯胜利了")
             break
-        elif ken_hp == chunli_hp:
+        elif ken_hp <= 0 and chunli_hp <= 0 :
             print("----------------------------")
             print("同归于尽")
-    print("============over==============")
+    print("============OVER==============")
 
 fight()
 
